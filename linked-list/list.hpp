c7d0e6d1575477c6efeefdcba3456cef40109be6
Isaac Maddox <idm10@students.uwf.edu>
2024-10-08 12:50:55 -0500

    More LinkedList constructors, thorough tests

--- linked-list/list.hpp
+++ linked-list/list.hpp
@@ -1,6 +1,7 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LinkedList_H
+#define LinkedList_H
 #include <iostream>
+#include <vector>
 
 template <typename T>
 class Node
@@ -13,12 +14,27 @@ public:
 };
 
 template <typename T>
-class List
+class LinkedList
 {
 private:
+   /**
+    * A pointer to the first element in the LinkedList
+    */
    Node<T> *head;
+   /**
+    * A pointer to the last element in the LinkedList
+    */
    Node<T> *tail;
+   /**
+    * The size of the linked LinkedList
+    */
    int size;
+   /**
+    * Retrieve a pointer to the node at a particular index
+    *
+    * @param idx The index of the node to find
+    * @throws std::out_of_range - If the index is invalid
+    */
    Node<T> *nodeAt(int idx)
    {
       if (idx > this->size - 1 || idx < 0)
@@ -37,7 +53,40 @@ private:
    }
 
 public:
-   List() : head(nullptr), tail(nullptr), size(0) {};
+   /**
+    * Create an empty linked list
+    */
+   LinkedList() : head(nullptr), tail(nullptr), size(0) {};
+
+   /**
+    * Create a linked list with an array of values
+    *
+    * @param values The array of values
+    * @param size The size of the array, which will become the size of the linked list
+    */
+   LinkedList(T *values, int size) : head(nullptr), tail(nullptr), size(0)
+   {
+      for (int i = 0; i < size; ++i)
+      {
+         this->append(values[i]);
+      }
+   }
+
+   LinkedList(std::vector<T> &values) : head(nullptr), tail(nullptr), size(0)
+   {
+      for (auto value : values)
+      {
+         this->append(value);
+      }
+   }
+
+   /**
+    * Get the value of the node at an index
+    *
+    * @param idx The index to retrieve the value of
+    * @returns The value of the node at index `idx`
+    * @throws std::out_of_range - If the specified index is invalid
+    */
    T at(int idx)
    {
       if (idx > this->size - 1 || idx < 0)
@@ -54,6 +103,13 @@ public:
 
       return val->value;
    }
+
+   /**
+    * Get the value of the last node in the linked list, also known as the tail
+    *
+    * @returns The value of the last node in the linked list
+    * @throws std::out_of_range - If the list is empty
+    */
    T last() const
    {
       if (this->size == 0)
@@ -63,6 +119,13 @@ public:
 
       return this->tail->value;
    }
+
+   /**
+    * Get the value of the first node in the linked list, also known as the head
+    *
+    * @returns The value of the first node in the linked list
+    * @throws std::out_of_range - If the list is empty
+    */
    T first() const
    {
       if (this->size == 0)
@@ -72,6 +135,12 @@ public:
 
       return this->head->value;
    }
+
+   /**
+    * Add a node to the beginning of the linked list
+    *
+    * @param value A pointer to the node to prepend
+    */
    void prepend(Node<T> *value)
    {
       if (this->head == nullptr)
@@ -89,10 +158,22 @@ public:
 
       ++this->size;
    }
+
+   /**
+    * Add a value to the front of the list
+    *
+    * @param value The value to add to the list
+    */
    void prepend(T value)
    {
       this->prepend(new Node<T>(value));
    }
+
+   /**
+    * Add a node to the end of the list
+    *
+    * @param value A pointer to the node to append
+    */
    void append(Node<T> *value)
    {
       if (this->tail == nullptr)
@@ -115,10 +196,20 @@ public:
 
       ++this->size;
    }
+
+   /**
+    * Add a value to the end of the list
+    *
+    * @param value The value to append
+    */
    void append(T value)
    {
       this->append(new Node<T>(value));
    }
+
+   /**
+    * Remove the node at a particular index
+    */
    void remove(int idx)
    {
       if (idx > this->size - 1 || idx < 0)
@@ -130,42 +221,81 @@ public:
       Node<T> *nextNode = currentNode->next;
       Node<T> *prevNode = currentNode->prev;
 
-      if (prevNode == nullptr)
+      if (prevNode == nullptr && nextNode != nullptr)
       {
          this->head = nextNode;
-         nextNode->prev = nullptr;
+         this->head->prev = nullptr;
       }
-      else
+      else if (prevNode != nullptr)
       {
          prevNode->next = nextNode;
       }
 
-      if (nextNode == nullptr)
+      if (nextNode == nullptr && prevNode != nullptr)
       {
          this->tail = prevNode;
          prevNode->next = nullptr;
       }
-      else
+      else if (nextNode != nullptr)
       {
          nextNode->prev = prevNode;
+      }
+
+      if (nextNode == nullptr && prevNode == nullptr)
+      {
+         this->head = nullptr;
+         this->tail = nullptr;
       }
 
       delete currentNode;
       --this->size;
    }
-   void print()
+
+   void clear()
+   {
+      while (this->head != nullptr)
+      {
+         this->remove(0);
+      }
+   }
+
+   /**
+    * Print the list out. This will print each item
+    * in the list on a newline.
+    */
+   void print() const
    {
       Node<T> *v = this->head;
 
       while (v != nullptr)
       {
-         std::cout << v->value << " " << std::endl;
+         std::cout << "  " << v->value << " ";
+         if (v != this->tail)
+         {
+            std::cout << std::endl;
+         }
          v = v->next;
       }
    }
+
+   /**
+    * Get the length of the linked list
+    *
+    * @returns The length of the list
+    */
    int length() const
    {
       return this->size;
+   }
+
+   /**
+    * Return whether or not the linked list is empty
+    *
+    * @returns Whether the list is empty
+    */
+   bool isEmpty() const
+   {
+      return this->size == 0;
    }
 };
 
--- linked-list/test-list.cpp
+++ linked-list/test-list.cpp
@@ -1,30 +1,68 @@
 #include "list.hpp"
 #include <iostream>
+#include <iomanip>
+#include <vector>
+
+template <typename T>
+void printList(LinkedList<T> &list, int lineWidth = 20)
+{
+   std::cout << std::endl
+             << std::setw(lineWidth) << std::setfill('-') << "" << std::endl;
+
+   if (!list.isEmpty())
+      list.print();
+   else
+      std::cout << "  List is empty";
+
+   std::cout
+       << std::endl
+       << std::setw(lineWidth) << std::setfill('-') << std::left << "" << std::endl;
+}
 
 int main()
 {
-   List<int> list;
+   using std::cout, std::endl;
+   LinkedList<int> list;
 
-   list.append(3);
-   list.append(4);
+   cout << "Empty List Constructor" << endl;
+
    list.append(5);
-   list.prepend(2);
-   list.prepend(1);
-   list.prepend(-200);
+   list.append(7);
+   list.prepend(6);
 
-   list.print();
+   printList(list);
 
-   std::cout << std::endl
-             << "Item at index 2: " << list.at(2) << std::endl
-             << std::endl;
+   list.clear();
 
-   list.remove(2);
+   printList(list);
 
-   list.print();
+   cout << endl
+        << "Array List Constructor" << endl;
 
-   std::cout << std::endl
-             << std::endl
-             << "List size is " << list.length() << std::endl;
+   LinkedList<std::string> list2(new std::string[2]{"Hello", "World"}, 2);
+
+   printList(list2);
+
+   list2.remove(1);
+
+   printList(list2);
+
+   cout << endl
+        << "Vector List Constructor" << endl;
+
+   std::vector<char> vec({'a', 'b', 'c', 'd'});
+
+   LinkedList<char> list3(vec);
+
+   printList(list3);
+
+   list3.prepend('i');
+
+   printList(list3);
+
+   list3.clear();
+
+   printList(list3);
 
    return EXIT_SUCCESS;
 }
